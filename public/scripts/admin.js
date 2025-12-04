8f637cb2d713ffdff6d394785257d46606d1fa88
Justice Ross <justice.ross2647@fvtc.edu>
2025-12-04 04:24:00 -0600

    Added loading events to home page, working POST for adding items and events via the Admin page, cleaned up code

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,57 +1,106 @@
-// Gets the input elements for the items
-const iNameInput = document.querySelector('#iName')
-const iDescInput = document.querySelector('#iDesc')
-const iPriceInput = document.querySelector('#iPrice')
-const iUrlInput = document.querySelector('#iUrl')
-const itemAddButton= document.querySelector('#itemAddButton')
+(async () => {
+    // Gets the input elements for the items
+    const iNameInput = document.querySelector('#iName')
+    const iDescInput = document.querySelector('#iDesc')
+    const iPriceInput = document.querySelector('#iPrice')
+    const iUrlInput = document.querySelector('#iUrl')
+    const itemAddButton= document.querySelector('#itemAddButton')
 
-// Gets the input elements for the events
-const eNameInput = document.querySelector('#eName')
-const eLocationInput = document.querySelector('#eLocation')
-const eDateInput = document.querySelector('#eDate')
-const eTimeInput = document.querySelector('#eTime')
-const eventAddButton= document.querySelector('#eventAddButton')
+    // Gets the input elements for the events
+    const eNameInput = document.querySelector('#eName')
+    const eLocationInput = document.querySelector('#eLocation')
+    const eDateInput = document.querySelector('#eDate')
+    const eTimeInput = document.querySelector('#eTime')
+    const eventAddButton= document.querySelector('#eventAddButton')
 
-// Functions for getting the Menu Items list and Events list (Temporary until I figure out how to get the number to increment based of the length of the DB tables)
-const getItems = async () => {
-    const response = await fetch('/api/v1/menu')
-    return await response.json()
-}
-const getEvents = async () => {
-    const response = await fetch('/api/v1/events')
-    return await response.json()
-}
+    // Gets the confirmation h2
+    const confirmationMessage = document.querySelector('.confirmation')
 
-let itemData = null
-let eventData = null
+    // Functions for getting the Menu Items list and Events list
+    const getItems = async () => {
+        const response = await fetch('/api/v1/menu')
+        return await response.json()
+    }
+    const getEvents = async () => {
+        const response = await fetch('/api/v1/events')
+        return await response.json()
+    }
 
-eventAddButton.addEventListener('click', async () => {
-    setEventInput()
-    const response = await fetch('/api/v1/events', {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventData)
+    // Variables for the input data
+    let itemData = null
+    let eventData = null
+
+    // Button click event for the POST of the menu items
+    itemAddButton.addEventListener('click', async () => {
+        setItemInput()
+        const response = await fetch('/api/v1/menu', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(itemData)
+        })
+
+        const result = await response.json()
+        confirmationMessage.textContent = "Item successfully added"
+            setTimeout(() => {
+                confirmationMessage.textContent = " "
+            }, 3000)
+        refreshData()
+        console.log(result)
+    })
+    
+    // Button click event for the POST of the events
+    eventAddButton.addEventListener('click', async () => {
+        setEventInput()
+        const response = await fetch('/api/v1/events', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(eventData)
+        })
+
+        const result = await response.json()
+        if(result !== null) {
+            confirmationMessage.textContent = "Event successfully added"
+            setTimeout(() => {
+                confirmationMessage.textContent = " "
+            }, 3000)
+        }
+        refreshData()
+        console.log(result)
     })
 
-    const result = await response.json()
-    console.log(result)
-    
-})
-
-//
-const setEventInput = () => {
-    eventData = {
-        number: 4,
-        eventName: eNameInput.value,
-        location: eLocationInput.value,
-        date: eDateInput.value,
-        time: eTimeInput.value
+    // Sets the items variable to the user input
+    const setItemInput = () => {
+        itemData = {
+            number: displayMenu.length + 1,
+            item: iNameInput.value,
+            description: iDescInput.value,
+            price: iPriceInput.value,
+            image: iUrlInput.value
+        }
     }
-}
 
-(async () => {
-    const displayMenu = await getItems()
-    const displayEvents = await getEvents()
+    // Sets the events variable to the user input
+    const setEventInput = () => {
+        eventData = {
+            number: displayEvents.length + 1,
+            eventName: eNameInput.value,
+            location: eLocationInput.value,
+            date: eDateInput.value,
+            time: eTimeInput.value
+        }
+    }
+
+    const refreshData = async () => {
+        // Refreshes the data
+        displayMenu = await getItems()
+        displayEvents = await getEvents()
+    }
+
+    // Variables used to get table length for the auto incremented Id
+    let displayMenu = await getItems()
+    let displayEvents = await getEvents()
 })()
--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,4 +1,5 @@
 const menuItems = document.querySelector(".menu-items")
+const events = document.querySelector(".events")
 
 // Functions for getting the Menu Items list and Events list
 const getItems = async () => {
@@ -11,9 +12,16 @@ const getEvents = async () => {
     return await response.json()
 }
 
+const getEvent = async id => {
+    const response = await fetch(`/api/v1/events/${id}`)
+    return await response.json()
+}
+
 // Adds items to the page
 const showMenuItems = displayMenu => {
     console.log("ShowMenu start")
+
+    // Creates a new containter for each menu item in the list
     displayMenu?.forEach(({item, description, price, image}) => {
         const menuItem = document.createElement("div")
         menuItem.className = "menu-item"
@@ -28,10 +36,58 @@ const showMenuItems = displayMenu => {
     })
 }
 
+const showEvents = displayEvents => {
+    console.log("ShowEvent start")
+
+    // Creates a new containter for each event in the list
+    displayEvents?.forEach(({number, eventName, date}) => {
+        const event = document.createElement("div")
+        event.className = "event"
+        event.innerHTML = `
+            <h2>${eventName}</h2>
+            <p>${date}</p>
+            <div class="eventInfo">
+                <p class="location"></p>
+                <p class="time"></p>
+            </div>
+            <p class="details">Click for more info...</p>
+        `
+
+        // Click event for the event divs
+        event.addEventListener('click', async () => {
+            // Gets the location and time using the event/id route
+            const { location, time } = await getEvent(number)
+            const showInfo = event.querySelector('.eventInfo')
+            const locationInfo = showInfo.querySelector('.location')
+            const timeInfo = showInfo.querySelector('.time')
+            const details = event.querySelector('.details')
+
+            // Sets the text content using the 
+            locationInfo.textContent = location
+            timeInfo.textContent = time
+
+            // Toggles the visibility of the eventInfo div 
+            if (showInfo.style.display === "block") {
+                showInfo.style.display = "none"
+                details.style.display = "block"
+            } else {
+                showInfo.style.display = "block"
+                details.style.display = "none"
+            }
+        })
+
+        events.appendChild(event)
+        console.log("ShowEvent end")
+    })
+}
+
 
 (async () => {
     const displayMenu = await getItems()
+    const displayEvents = await getEvents()
     console.log(displayMenu)
+    console.log(displayEvents)
     showMenuItems(displayMenu)
+    showEvents(displayEvents)
 })()
 
